b236d52cd266750f83097d304972b27b396eb5cd
konerunatwar <konerunatwar@gmail.com>
2021-05-15 12:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,12 +65,12 @@ def predict_rent(data:value):
     lift= data['lift']
     swimmingPool= data['swimmingPool']
     location= data['location']
-    Deviceid = data['Deviceid']
+    deviceid = data['Deviceid']
     prediction = int(get_estimated_price(property_size,bhk,property_age,gym,lift,swimmingPool,location))
     
     endpoint='https://api.airtable.com/v0/appnaWXwqsABs8iyJ/Table%201'
     headers = {"Authorization": "Bearer key5g3yXFYHMFX4R0","Content-Type": "application/json"}
-    data = {"records": [{"fields": {"propertysize": property_size,"bhk": bhk,"propertyage": property_age,"gym": gym,"lift": lift,"swimmingPool": swimmingPool,"location":location,"prediction":prediction,"Deviceid":Deviceid}}]}
+    data = {"records": [{"fields": {"propertysize": property_size,"bhk": bhk,"propertyage": property_age,"gym": gym,"lift": lift,"swimmingPool": swimmingPool,"location":location,"prediction":prediction,"Deviceid":deviceid}}]}
     r = requests.post(endpoint, json=data, headers=headers)
     return {
         'prediction': prediction
